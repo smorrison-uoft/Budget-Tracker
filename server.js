381e4127043d70d8004da084bcc0d979f4bcd8a1
stephanie morrison <stephmorr.to@gmail.com>
2020-10-15 15:28:05 -0400

    deploying

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/Budget Tracker',
+    process.env.MONGODB_URI || 'mongodb://localhost/BudgetTracker',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
